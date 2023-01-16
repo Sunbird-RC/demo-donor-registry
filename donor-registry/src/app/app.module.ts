53fe340a36844b9bddf63f63aa87a977b8fb4147
Pratiksha Khandagale <pratiksha_k@techjoomla.com>
2023-01-16 11:07:13 +0530

    issue fixed

--- donor-registry/src/app/app.module.ts
+++ donor-registry/src/app/app.module.ts
@@ -123,7 +123,7 @@ import { DocDetailViewComponent } from './documents/doc-detail-view/doc-detail-v
 import { SafeHtmlPipe } from './safe-html.pipe';
 import { initTheme } from './theme.config';
 import { VerifyIndentityCode } from './forms/types/verify-identity-no.type';
-import { FormlyFieldSelect } from './forms/types/select.type';
+// import { FormlyFieldSelect } from './forms/types/select.type';
 // import { CreateCertificateComponent } from './create-certificate/create-certificate.component';
 
 @NgModule({
@@ -221,8 +221,7 @@ import { FormlyFieldSelect } from './forms/types/select.type';
         { name: 'file', component: FormlyFieldFile, wrappers: ['form-field'] },
         { name: 'multiselect', component: FormlyFieldNgSelect },
         { name: 'color', component: FormlyColorInput },
-        { name: 'verify-code', component: VerifyIndentityCode },
-        { name: 'select1', component: FormlyFieldSelect }
+        { name: 'verify-code', component: VerifyIndentityCode }
       ],
     }),
     ToastrModule.forRoot({
