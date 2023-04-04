e680b616b3820fa86c32bb2acc33e391c6d7d729
Pratiksha Khandagale <pratiksha_k@techjoomla.com>
2023-04-04 15:20:20 +0530

    Issue #140 fix: Error Message - mobile number is entered multiple times

--- donor-registry/src/app/forms/types/verify-mobileno.type.ts
+++ donor-registry/src/app/forms/types/verify-mobileno.type.ts
@@ -22,7 +22,7 @@ export class VerifyMobileNo extends FieldType {
   dataObj: any;
   isNumberValid: boolean = true;
   errorMessage: any;
-  customErrCode: string;
+  customErrCode: string = '';
   err401: boolean = false;
   noLinkedAbha: boolean = false;
   fieldKey: any;
@@ -66,6 +66,8 @@ export class VerifyMobileNo extends FieldType {
           //  (<HTMLInputElement>document.getElementById(fieldKey)).value = "";
             this.selectProfile();
             this.noLinkedAbha = true;
+            this.checkErrType(error);
+
             console.log(error);
           }
         });
@@ -84,12 +86,15 @@ export class VerifyMobileNo extends FieldType {
   }
 
   checkErrType(err) {
+   
     this.errorMessage = err?.error['message'];
-    if (this.errorMessage.includes('30')) {
+    if (this.errorMessage != undefined && this.errorMessage.includes('30')) {
       this.customErrCode = '420';
-    }
-    if (this.errorMessage.includes('enter valid mobile')) {
+      this.errorMessage = "";
+    }else if (this.errorMessage != undefined && this.errorMessage.includes('enter valid mobile')) {
       this.customErrCode = '427';
+    }else{
+      this.customErrCode = '';
     }
   }
 
@@ -150,7 +155,6 @@ export class VerifyMobileNo extends FieldType {
             this.err401 = true;
           }
 
-          //document.getElementById('closeModalButton').click();
           console.error('There was an error!', error);
         },
       });
