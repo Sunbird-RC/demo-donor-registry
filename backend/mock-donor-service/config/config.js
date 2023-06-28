1259e8ed352d08fb1ad7107d961b062402b0bb31
Tejas Varade <varadetejas@gmail.com>
2023-06-28 13:02:24 +0530

    config for redis and mock token url for abha apis

---
+++ backend/mock-donor-service/config/config.js
@@ -0,0 +1,4 @@
+const REDIS_URL = process.env.REDIS_URL;
+module.exports = { 
+    REDIS_URL
+}
--- backend/mock-donor-service/main.js
+++ backend/mock-donor-service/main.js
@@ -6,13 +6,13 @@ const {esignRouter} = require('./src/routers/esign.router');
 const {notifyRouter} = require('./src/routers/notify.router');
 const { initRedis } = require('./src/services/redis.service');
 const { PORT } = require('./config/constants');
+const { REDIS_URL } = require('./config/config');
 
 (async() => {
-    await initRedis({REDIS_URL: 'redis://localhost:6379'})
+    await initRedis({REDIS_URL: REDIS_URL})
 })();
 app.use((bodyParser.json()));
 app.use('', abhaRouter);
 app.use('', esignRouter);
 app.use('', notifyRouter);
-// app.use(express.json())
 app.listen(PORT);
