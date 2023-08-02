81dea1b4945043a9a176b8bbeed9097328a175c3
Holash Chand <holash.c@beehyv.com>
2023-08-02 13:25:26 +0530

    fixed photo null issue

--- backend/donor-service/main.js
+++ backend/donor-service/main.js
@@ -67,11 +67,11 @@ const getProfileFromUserAndRedis = (profileFromReq, profileFromRedis) => {
         profile.personalDetails.fatherName = profileFromRedis.fatherName;
     }
     profile.personalDetails.dob = (`${profileFromRedis.yearOfBirth}-${String(profileFromRedis.monthOfBirth).padStart(2, '0')}-${String(profileFromRedis.dayOfBirth).padStart(2, '0')}`);
-    profile.personalDetails.gender = GENDER_MAP[profileFromRedis.gender];
-    profile.personalDetails.photo = profileFromRedis.profilePhoto;
-    profile.addressDetails.state = toTitleCase(profileFromRedis.stateName);
-    profile.addressDetails.district = profileFromRedis.districtName;
-    profile.addressDetails.pincode = profileFromRedis.pincode;
+    profile.personalDetails.gender = GENDER_MAP[profileFromRedis.gender] || "Male";
+    profile.personalDetails.photo = profileFromRedis.profilePhoto || "";
+    profile.addressDetails.state = toTitleCase(profileFromRedis.stateName) || "";
+    profile.addressDetails.district = profileFromRedis.districtName || "";
+    profile.addressDetails.pincode = profileFromRedis.pincode || "";
     profile.identificationDetails.abha = String(profileFromRedis.healthIdNumber).replace(/-/g, '');
     return profile;
 }
