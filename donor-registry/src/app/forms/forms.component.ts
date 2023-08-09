2b4f8e34badac23f6fcbe0783773981daed1b93f
Chaitrali R <chaitrali_r@tekditechnologies.com>
2023-08-09 12:23:04 +0530

    Form Disable Changes

--- donor-registry/src/app/forms/forms.component.ts
+++ donor-registry/src/app/forms/forms.component.ts
@@ -245,11 +245,11 @@ export class FormsComponent implements OnInit {
               localStorage.setItem('isAutoFill', "false");
 
               let obj = { ...this.model['personalDetails'], ...this.model['addressDetails'] };
-              for (let propName in obj) {
-                if (obj[propName] === null || obj[propName] === undefined || obj[propName] === "") {
-                  delete obj[propName];
-                }
-              }
+              // for (let propName in obj) {
+              //   if (obj[propName] === null || obj[propName] === undefined || obj[propName] === "") {
+              //     delete obj[propName];
+              //   }
+              // }
               localStorage.setItem('notReadOnly', JSON.stringify(Object.keys(obj)));
             }
           }
@@ -350,11 +350,11 @@ export class FormsComponent implements OnInit {
       let notReadOnly = localStorage.getItem('notReadOnly');
       if (!notReadOnly || notReadOnly === "[]") {
         let obj = { ...this.model['personalDetails'], ...this.model['addressDetails'] };
-        for (let propName in obj) {
-          if (obj[propName] === null || obj[propName] === undefined || obj[propName] === "") {
-            delete obj[propName];
-          }
-        }
+        // for (let propName in obj) {
+        //   if (obj[propName] === null || obj[propName] === undefined || obj[propName] === "") {
+        //     delete obj[propName];
+        //   }
+        // }
         localStorage.setItem('notReadOnly', JSON.stringify(Object.keys(obj)));
       }
       localStorage.setItem('isVerified', 'true')
