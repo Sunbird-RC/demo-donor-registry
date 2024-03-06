250f493426ab3427c587d952c87b711c582100f3
Sreejit-K <kanjarla.sreejit@gmail.com>
2024-03-06 19:49:59 +0530

    Added randomMockProfileGeneration logic and neccessary changes for esign UI changes and added Domain_URL variable

--- backend/mock-donor-service/config/constants.js
+++ backend/mock-donor-service/config/constants.js
@@ -1,7 +1,8 @@
 const HOST = process.env.HOST || 'localhost'
 const PROTOCOL = process.env.PROTOCOL || 'http'
 const PORT = process.env.PORT || 5001
+const DOMAIN_URL = process.env.DOMAIN_URL || 'http://localhost:5001'
 
 module.exports = {
-    PROTOCOL, HOST, PORT
+    PROTOCOL, HOST, PORT, DOMAIN_URL
 }
--- backend/mock-donor-service/src/services/mock-profile.service.js
+++ backend/mock-donor-service/src/services/mock-profile.service.js
@@ -4,12 +4,37 @@ const getRandomMockProfile = (abha = null) => {
     if(abha === null) {
         const range = randomProfiles.length;
         const index = Math.floor(Math.random() * range);
+        randomProfiles[index]["healthIdNumber"] = generateRandomABHA();
+        randomProfiles[index]["mobile"] = generateRandomMobileNumber();
         return randomProfiles[index];
     } else if(randomProfiles.filter(profile => profile.healthIdNumber.replace(/-/g, '') === abha).length > 0) {
         return randomProfiles.filter(profile => profile.healthIdNumber.replace(/-/g, '') === abha)[0]
     } 
     return null;
 }
+
+
+function generateRandomABHA() {
+    let result = '';
+    // Generate random prefix in the range of 10-99
+    const prefix = Math.floor(Math.random() * 90) + 10;
+    result += prefix + '-';
+    // Generate random numbers in the format xxxx-xxxx-xxxx
+    for (let i = 0; i < 3; i++) {
+        result += Math.floor(Math.random() * 10000).toString().padStart(4, '0');
+        if (i < 2) result += '-';
+    }
+    return result;
+}
+
+function generateRandomMobileNumber() {
+    let phoneNumber = '9'; // Start with 9 to ensure it's a valid mobile number
+    for (let i = 1; i < 10; i++) {
+        phoneNumber += Math.floor(Math.random() * 10);
+    }
+    return phoneNumber;
+}
+
 
 const isMockProfilePresent = (abha) => {
     return randomProfiles.filter(profile => profile.healthIdNumber.replace(/-/g, '') === abha).length > 0
@@ -19,5 +44,6 @@ const isMockProfilePresent = (abha) => {
 
 module.exports = {
     getRandomMockProfile,
-    isMockProfilePresent
+    isMockProfilePresent,
+    generateRandomABHA
 }
